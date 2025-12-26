d50f12fc1d1d4d3b5cac97b0cfb3587d5bfc9998
Brent Guffens <16427929+brentguf@users.noreply.github.com>
2025-12-26 13:15:21 +0100

    Better DX: use lodash-es instead of lodash

--- app/javascript/pages/Followers/Index.tsx
+++ app/javascript/pages/Followers/Index.tsx
@@ -1,5 +1,5 @@
 import { usePage } from "@inertiajs/react";
-import debounce from "lodash/debounce";
+import { debounce } from "lodash-es";
 import * as React from "react";
 import { cast } from "ts-safe-cast";
 
